b61ff8b0d424cd86a4bce6e99b13698b3395b610
ellie turnock <ebturnock@gmail.com>
2019-02-10 17:31:12 +0000

    Unit: finalScore function calculates all rolls

--- spec/units/game.unit.test.js
+++ spec/units/game.unit.test.js
@@ -9,16 +9,23 @@ beforeAll(() => {
 });
 
 describe('#game', () => {
-
   test('starts with an empty game', () => {
     expect(scorecard.game).toEqual([]);
   });
 });
 
 describe('#roll', () => {
-
   test('stores the score in the scorecard', () => {
     scorecard.inputRoll(6)
     expect(scorecard.game).toEqual([6]);
+  });
+});
+
+describe('#finalScore', () => {
+  test('calculates score for whole game', () => {
+    for (let i = 0; i <= 20; i++) {
+      scorecard.inputRoll(1);
+    }
+    expect(scorecard.finalScore).toEqual(20);
   });
 });
