9beb9a1c0c80355ba438037c6f98456a0f6a3620
ellie turnock <ebturnock@gmail.com>
2019-02-10 17:12:21 +0000

    Unit: roll function stores score in scorecard

--- spec/units/game.unit.test.js
+++ spec/units/game.unit.test.js
@@ -2,15 +2,23 @@
 
 const Scorecard = require('bowling-challenge/scorecard.js')
 
-describe('game', () => {
+let scorecard;
 
-  let scorecard;
+beforeAll(() => {
+  scorecard = new Scorecard();
+});
 
-  beforeEach(() => {
-    scorecard = new Scorecard();
-  });
+describe('#game', () => {
 
   test('starts with an empty game', () => {
     expect(scorecard.game).toEqual([]);
+  });
+});
+
+describe('#roll', () => {
+
+  test('stores the score in the scorecard', () => {
+    scorecard.inputRoll(6)
+    expect(scorecard.game).toEqual([6]);
   });
 });
