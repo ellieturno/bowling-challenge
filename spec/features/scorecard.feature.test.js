295a65c326c5ee8027721ddd3f488c4be60d5382
ellie turnock <ebturnock@gmail.com>
2019-02-10 18:36:03 +0000

    Feature: player can roll a spare and get bonus points from their next roll

--- spec/features/scorecard.feature.test.js
+++ spec/features/scorecard.feature.test.js
@@ -24,4 +24,12 @@ describe('Scorecard Features:', () => {
     scorecard.inputRoll(4);
     expect(scorecard.game).toEqual([[1,2],[3,4]]);
   })
+
+  test('player rolls a spare and gets a bonus', () => {
+    scorecard.inputRoll(8);
+    scorecard.inputRoll(2);
+    scorecard.inputRoll(3);
+    scorecard.inputRoll(1);
+    expect(scorecard.finalScore()).toEqual(17);
+  });
 });
