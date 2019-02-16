4f0e7fc481f3c55492b1f2005a3a562af2f3f8ba
ellie turnock <ebturnock@gmail.com>
2019-02-16 17:41:53 +0000

    isSpare function checks if frame === 10

--- scorecard.js
+++ scorecard.js
@@ -1,27 +1,32 @@
-'use-strict';
+'use-strict'
 
 class Scorecard {
-  constructor(game = [], frame = []) {
-    this.game = game;
-    this.frame = frame;
+  constructor () {
+    this.game = []
+    this.frame = []
   };
 
-  inputRoll(score) {
+  inputRoll (score) {
     this.frame.push(score)
     if (this.frame.length === 2) {
       this.game.push(this.frame)
       this.frame = []
-    }
+      this.frameIndex += 1
+    };
   };
 
-  finalScore() {
+  finalScore () {
     var total = 0
-    this.game.forEach(function(frame) {
+    this.game.forEach(function (frame) {
       total += frame.reduce((accumulator, rollScore) => {
         return accumulator + rollScore
-      });
-    });
+      })
+    })
     return total
+  };
+
+  isSpare () {
+    return this.game[0][0] + this.game[0][1] === 10;
   };
 };
 
